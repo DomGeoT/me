dbb12c20227adbb8f28c120299358d403982facd
Dominic  Taylor <dominictaylor@Dominics-MacBook-Air.local>
2024-02-09 08:39:46 +0900

    feat: update post api

--- src/app/api/trips/[tripId]/route.ts
+++ src/app/api/trips/[tripId]/route.ts
@@ -35,6 +35,81 @@ export async function GET(
     return NextResponse.json({ trip }, { status: 200 })
 }
 
+export async function PATCH(
+    request: Request,
+    { params }: { params: { tripId: string } }
+) {
+    const trip = await request.formData()
+
+    // if (
+    //     !trip.has("heading") ||
+    //     !trip.has("description") ||
+    //     !trip.has("rawMarkdownContent") ||
+    //     !trip.has("longitude") ||
+    //     !trip.has("latitude") ||
+    //     !trip.has("image")
+    // ) {
+    //     return NextResponse.json({}, { status: 400 })
+    // }
+    const client = await clientPromise
+    const db = client.db()
+    // const s3 = new S3Client({
+    //     region: "eu-north-1",
+    // })
+
+    // await s3.config.credentials({ forceRefresh: true })
+
+    // const imageUrls: string[] = []
+
+    // for (const file of trip.getAll("image")) {
+    //     const imageFile = file as File
+
+    //     const formattedImageFile = await sharp(await imageFile.arrayBuffer())
+    //         .resize(1920)
+    //         .webp({ quality: 80 })
+    //         .toBuffer()
+
+    //     const id = crypto.randomBytes(24).toString("hex")
+
+    //     const input = {
+    //         Body: formattedImageFile,
+    //         Bucket: "domgeot-website-images",
+    //         Key: id,
+    //     }
+
+    //     const uploadCommand = new PutObjectCommand(input)
+    //     const res = await s3.send(uploadCommand)
+
+    //     if (res.$metadata.httpStatusCode !== 200) {
+    //         console.error("Failed to upload image", res)
+    //         continue
+    //     }
+
+    //     const publicUrl = `https://domgeot-website-images.s3.eu-north-1.amazonaws.com/${id}`
+    //     imageUrls.push(publicUrl)
+    // }
+
+    const tripId = new ObjectId(params.tripId)
+
+    const _trip = await db.collection<TripShape>("trips").updateOne(
+        // @ts-expect-error not sure why typing fails here
+        { _id: tripId },
+        {
+            heading: trip.get("heading") ?? undefined,
+            description: trip.get("description") ?? undefined,
+            rawMarkdownContent: trip.get("rawMarkdownContent") ?? undefined,
+            // images: imageUrls,
+            longitude: trip.get("longitude") ?? undefined,
+            latitude: trip.get("latitude") ?? undefined,
+        }
+    )
+
+    if (_trip.matchedCount === 0) {
+        return NextResponse.json({}, { status: 503 })
+    }
+    return NextResponse.json({}, { status: 200 })
+}
+
 export async function DELETE(
     request: Request,
     { params }: { params: { tripId: string } }
